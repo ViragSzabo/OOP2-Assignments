2bf395b4444d23546a798e218dd7a90d51444ae4
ViragSzabo <virag.szabo@student.nhlstenden.com>
2024-10-17 14:43:11 +0200

    Updates

---
+++ src/test/java/ExamPreparation/Implementation/LibrarySystem/Transaction/BorrowTransactionTest.java
@@ -0,0 +1,9 @@
+package ExamPreparation.Implementation.LibrarySystem.Transaction;
+
+import junit.framework.TestCase;
+
+public class BorrowTransactionTest extends TestCase {
+
+    public void testBorrowBooks() {
+    }
+}
